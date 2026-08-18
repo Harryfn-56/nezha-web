395ee49a26d826b33eab36d8c5adb7afa36ddc80
Harry <quanganhh5612@gmail.com>
2026-08-18 21:54:17 +0700

    Cap nhat cau hinh Supabase

--- public/js/config.js
+++ public/js/config.js
@@ -51,8 +51,8 @@ export const CONFIG = {
    *      B4. npm run build  →  tải lại dist/ lên Hostinger
    * ---------------------------------------------------------------- */
   supabase: {
-    url: '',
-    anonKey: '',
+    url: 'https://bgduzfskeqfnboytktus.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnZHV6ZnNrZXFmbmJveXRrdHVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3OTQxMTEsImV4cCI6MjEwMjM3MDExMX0.oU89B3qG0GJlJ1Zup3l0T9_Ttrch4vWgo712oJ2ETqU',
   },
 
   /* ------------------------------------------------------------------
